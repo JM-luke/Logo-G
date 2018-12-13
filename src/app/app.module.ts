9af0b2bbf0bda712c258b973af47f0a1441c1871
José Manuel Luque <jwluque@gmail.com>
2018-12-13 23:39:33 +0100

    añadir material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 //import { from } from 'rxjs';
 
+//Animations
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Material
+import { MaterialModule } from './material';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
